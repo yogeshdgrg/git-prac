b5a5bd3522ffce6ce6576301cc34834523d8e839
Yogesh Gurunng <yogeshgurung667@gmail.com>
2024-12-05 18:04:49 +0545

    Added some content in a feat branch

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 console.log("Hello I am coming home...")
+console.log("Ma dherai Khusi xu..")
 console.log("I am going back from the home...")
 
 let a = 10
@@ -6,6 +7,8 @@ let b = 2
 
 const mul = a * b
 console.log("The multiplication of a and b is : ", mul)
+
+console.log("I am in a good mood today.")
 
 console.log(
   "Hey Good to hear that You are the most amazing person I have ever seen in my life"
