63400f99794464b3395574377b5deff4b794eb37
Yogesh Gurunng <yogeshgurung667@gmail.com>
2024-12-05 17:41:11 +0545

    Subtract operation added.

--- home.js
+++ home.js
@@ -1,2 +1,8 @@
 console.log("Hello I am coming home...")
 console.log("I am going back from the home...")
+
+let a = 10
+let b = 2
+
+const diff = a - b
+console.log("The difference between a and b is : ", diff)
